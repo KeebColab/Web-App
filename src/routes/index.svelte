135b64093f1b9ae5e4bcc2b67f41d97462a4ebc7
James Hart <jhart2083@gmail.com>
2021-11-19 08:06:53 +0100

    feat: :bug: set nft form to homepage
    changed the template routing and commented out the header and footer including css

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,59 +1,138 @@
 <script context="module">
-	export const prerender = true;
+  export const prerender = true;
 </script>
 
 <script>
-	import Counter from '$lib/Counter.svelte';
+  // Gray 100 theme
+  import "carbon-components-svelte/css/g80.css";
+  import {
+    Form,
+    FormGroup,
+    TextInput,
+    Checkbox,
+    RadioButtonGroup,
+    RadioButton,
+    Select,
+    SelectItem,
+    Button,
+  } from "carbon-components-svelte";
+  function handleSubmit(event) {
+    // Prevent the default form submission
+    // event.preventDefault();
+    // console log the form data
+    console.log(event.target.apiKey.value);
+    console.log(event.target.owner.value);
+    console.log(event.target.name.value);
+    console.log(event.target.symbol.value);
+    console.log(event.target.chain.value);
+    console.log(event.target.updatable.value);
+    const data = JSON.stringify({
+      chain: event.target.chain.value,
+      name: event.target.name.value,
+      symbol: event.target.symbol.value,
+      owner_address: event.target.owner.value,
+      metadata_updatable: event.target.updatable.value,
+    });
+
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === this.DONE) {
+        console.log(this.responseText);
+      }
+    });
+
+    xhr.open("POST", "https://api.nftport.xyz/v0/contracts");
+    xhr.withCredentials = false;
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Authorization", event.target.apiKey.value);
+
+    xhr.send(data);
+  }
 </script>
 
-<svelte:head>
-	<title>Home</title>
-</svelte:head>
-
 <section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
+  <h1>
+    Welcome to your new<br />SvelteKit app
+  </h1>
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
+  <h2>
+    try editing <strong>src/routes/index.svelte</strong>
+  </h2>
 </section>
 
+<div>
+  <Form on:submit={handleSubmit}>
+    <TextInput
+      name="apiKey"
+      labelText="NFTPort API Key"
+      placeholder="Enter api key..."
+    />
+    <TextInput
+      name="owner"
+      labelText="Your Ethereum Address"
+      placeholder="0x..."
+    />
+    <TextInput
+      name="name"
+      labelText="Name your Contract"
+      placeholder="Enter story name..."
+    />
+    <TextInput name="symbol" labelText="Symbol" placeholder="Enter symbol..." />
+    <FormGroup>
+      <Select name="chain" labelText="Select a blockchain" value="polygon">
+        <!-- <SelectItem
+          hidden
+          value="placeholder"
+          text="Choose a blockchain"
+        /> -->
+        <SelectItem value="polygon" text="Polygon" />
+        <SelectItem value="rinkeby" text="Rinkeby Testnet" />
+      </Select>
+      <Select name="updatable" labelText="Updatable Metadata" value="polygon">
+        <!-- <SelectItem
+          hidden
+          value="placeholder"
+          text="Choose a blockchain"
+        /> -->
+        <SelectItem value="true" text="Yes, I want to have dynamic NFTs." />
+        <SelectItem
+          value="false"
+          text="No, I want my Metadata frozen once minted."
+        />
+      </Select>
+    </FormGroup>
+
+    <Button type="submit">Submit</Button>
+  </Form>
+</div>
+
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
+  /* section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+  }
 
-	h1 {
-		width: 100%;
-	}
+  h1 {
+    width: 100%;
+  }
 
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
+  .welcome {
+    position: relative;
+    width: 100%;
+    height: 0;
+    padding: 0 0 calc(100% * 495 / 2048) 0;
+  }
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+  .welcome img {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    display: block;
+  } */
 </style>
